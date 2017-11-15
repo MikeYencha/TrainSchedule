76e89ef0e5939ef074a969574ee18ab9a5029a29
John Yencha III <mikeydoughnuts@Johns-MacBook-Pro.local>
2017-11-15 17:56:38 -0500

    moment.js conquered!  almost

--- scripts/main.js
+++ scripts/main.js
@@ -13,39 +13,55 @@ var database = firebase.database();
 var trainName = '';
 var destination = '';
 var frequency = '';
+var firstTime = '';
+//setting variables for time arrival
 var arrivalTime = '';
 var minutesAway = '';
+
 //collect data on click
 $('#submit-train').on('click',function(){
     trainName = $('#new-train-name').val().trim();
     destination = $('#new-destination').val().trim();
     frequency = $('#new-frequency').val().trim();
-    arrivalTime = $('#new-arrival-time').val().trim();
+    firstTime = $('#new-first-time').val().trim();
     //keep adding information to the database
     database.ref().push({
       trainName:trainName,
       destination:destination,
-      arrivalTime:arrivalTime,
+      firstTime:firstTime,
       frequency:frequency,
       dateAdded:firebase.database.ServerValue.TIMESTAMP
     })
   });
-  //add subsequesnt entries by user to the bottom of the table
-  database.ref().orderByChild('dateAdded').limitToLast(1).on('value', function(snapshot) {
-    $('#train-body').html(
-        '<tr><td>'+snapshot.val().trainName
-        +'<td>'+snapshot.val().destination+'</td>'
-        +'<td>'+snapshot.val().frequency+'</td>'
-        +'<td>'+snapshot.val().arrivalTime+'</td>'
-        +'<td>'+snapshot.val().minutesAway+'</td>'
-      )
+// moment.js function
+function nextTrain(interval,startTime){
+    var time = moment(startTime,"HH:mm").format("HH:mm");
+    var tnow = moment(moment.now()).format("HH:mm");
+    if (time < tnow){
+      var nt = moment(time,"HH:mm").add(interval,"m").format("HH:mm");
+      return nextTrain(interval, nt);
+    }
+    return time;
+  }
+
+  //add data for subsequent entries to be stored as next entry in database
+  database.ref().orderByChild('dateAdded').limitToLast(1).on('child_added', function(snapshot) {
+      trainName = snapshot.val().trainName;
+      destination = snapshot.val().destination;
+      frequency = snapshot.val().frequency;
+      firstTime = snapshot.val().firstTime;
   });
+  
   //create table row based on child-added feqturels
   database.ref().on('child_added',function(snapshot){
+      var nextRun = nextTrain(snapshot.val().frequency, snapshot.val().firstTime);
+      nextRun = moment(nextRun, "HH:mm").format("hh:mm a");
+    //   var away = 
       var tableRow = '<tr><td>'+snapshot.val().trainName+'</td>'
       +'<td>'+snapshot.val().destination+'</td>'
       +'<td>'+snapshot.val().frequency+'</td>'
-      +'<td>'+snapshot.val().arrivalTime+'</td>'
-      +'<td>'+snapshot.val().minutesAway+'</td>';
+      +'<td>'+nextRun+'</td>'
+      +'<td>'+ +'</td>';
       $('#train-body').append(tableRow);
   });
+//snapshot.val().arrivalTime
